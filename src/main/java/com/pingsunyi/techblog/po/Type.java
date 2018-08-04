f789a221c7662250f4ac8849ed267f3a1c0480f7
pingsunyi <psydd@outlook.com>
2018-08-04 01:29:03 -0400

    creating entity classes

---
+++ src/main/java/com/pingsunyi/techblog/po/Type.java
@@ -0,0 +1,54 @@
+package com.pingsunyi.techblog.po;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "t_type")
+public class Type {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "type")
+    private List<Blog> blog = new ArrayList<>();
+
+    public Type() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Blog> getBlog() {
+        return blog;
+    }
+
+    public void setBlog(List<Blog> blog) {
+        this.blog = blog;
+    }
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
